1986fffbbf61d3d9da3fe0878e725441bceaad1e
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-27 03:21:02 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,39 +12,39 @@ end
 
 
 def monopoly
-	monopoly = {:railroads => {
-	    
-	                    :pieces => 4,
-	                    :names => {
-	                        :reading_railroad => {
-	                            "mortage_value" => "$100"
-	                        },
-	                        :pennsylvania_railroad => {
-	                            "mortage_value" => "$200"
-	                        },
-	                        :b_and_o_railroad => {
-	                            "mortage_value" => "$400"
-	                        },
-	                        :shortline => {
-	                            "mortage_value" => "$800"
-	                        }
-	                      
-	                    }, #end names hash
-	                    :rent_in_dollars => {
-	                        :one_piece_owned => 25,
-	                        :two_piece_owned => 50,
-	                        :three_piece_owned => 100,
-	                        :four_piece_owned => 200
-	                      }#ends rent in dollars hash
-	                      
-	                      
-	                      
-	                } #ends railroads hash
-	
-	  
-	  
-	  
-	}
+	monopoly = {
+	    :railroads => 
+	    {
+	           :pieces => 4,
+	           :names => {
+	                 :reading_railroad => 
+	                 {
+	                    "mortage_value" => "$100"
+	                  },
+	                 :pennsylvania_railroad => 
+	                 {
+	                     "mortage_value" => "$200"
+	                  },
+	                 :b_and_o_railroad => 
+	                 {
+	                     "mortage_value" => "$400"
+	                  },
+	                 :shortline => 
+	                 {
+	                     "mortage_value" => "$800"
+	                  }
+	             }, #end names hash
+	             
+	             
+	            :rent_in_dollars => {
+	                   :one_piece_owned => 25,
+	                   :two_piece_owned => 50,
+	                   :three_piece_owned => 100,
+	                   :four_piece_owned => 200
+	             }#ends rent in dollars hash
+
+	       } #ends railroads hash
+	} #end monopoly hash
 
 end
 
