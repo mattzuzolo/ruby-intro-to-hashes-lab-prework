9683059a1084c972a88ce85677a0202fd2865aed
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-27 03:15:01 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,10 @@ def monopoly
 	    
 	                    :pieces => 4,
 	                    :names => {
-	                        :
+	                        :reading_railroad => {}
+	                        :pennsylvania_railroad => {}
+	                        :b_and_o_railroad => {}
+	                        :shortline => {}
 	                      
 	                    }, #end names hash
 	                    :rent_in_dollars => {
