7787f2191b0d2e3d080a01170a95892c32dbf9c3
Matt Zuzolo <mattzuzolo@outlook.com>
2018-04-27 03:23:40 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,9 +22,9 @@ def monopoly_with_second_tier
                 :names => {},
                 :rent_in_dollars => {
                   :one_piece_owned => 25,
-	                   :two_piece_owned => 50,
-	                   :three_piece_owned => 100,
-	                   :four_piece_owned => 200
+	                :two_piece_owned => 50,
+	                :three_piece_owned => 100,
+	                :four_piece_owned => 200
                 }
               } 
     
